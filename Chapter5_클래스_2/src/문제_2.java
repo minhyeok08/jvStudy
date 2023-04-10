2a2be97963ebadab66d92d1efd95b74fafff8c06
SIST <SIST@DESKTOP-360I4IK>
2023-04-10 17:53:22 +0900

    1111

---
+++ Chapter5_클래스_2/src/문제_2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class 문제_2 {
+	static void numPrint()
+	{
+		for(int i=1;i<=10;i++)
+		{
+			System.out.print(i+" ");
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		numPrint();
+	
+	}
+
+}
