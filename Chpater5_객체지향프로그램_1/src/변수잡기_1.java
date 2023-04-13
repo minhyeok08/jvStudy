59eaca97802f50b83ee72197ef78194d07421573
SIST <SIST@DESKTOP-360I4IK>
2023-04-14 08:55:54 +0900

    1111

---
+++ Chpater5_객체지향프로그램_1/src/변수잡기_1.java
@@ -0,0 +1,90 @@
+class 쥬라기월드
+{
+	String Poster;
+	String nameKor;
+	String nameEng;
+	String date;
+	String type;
+	String nation;
+	int runTime;
+	double score;
+	int rank;
+	String story;
+}
+
+class News
+{
+	String poster;
+	String title;
+	String content;
+	String date;
+	String comNews;
+}
+
+class RecomendRes
+{
+	String poster;
+	String title;
+	String content;
+}
+
+class DetailRes
+{
+	String name;
+	double score;
+	String address, address2;
+	String tel;
+	String type;
+	String price;
+	String parking;
+	String time;
+	String menu; // 5개 묶음 배열
+}
+
+class ReciFood
+{
+	String poster;
+	String title;
+	String profile;
+	String nickname;
+	String starScore;
+	int rank;
+	int hit;
+}
+
+class RankMusic
+{
+	int rank;
+	int upDown;
+	String poster;
+	String title;
+	String singer;
+	String album;
+	int heartCount;
+}
+
+class SmartStore
+{
+	int dc;
+	String poster;
+	String menuName;
+	int price;
+}
+
+class InformationPlay
+{
+	String poster;
+	String title;
+	String play;
+	String type;
+	String date;
+	boolean inShow;
+}
+public class 변수잡기_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] poster = new String[3];
+	}
+
+}
